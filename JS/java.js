0b248feb42a0a0869aab12551426d2fedbb35ae4
yulia <yulianperezsambony@gmail.com>
2023-06-02 21:18:45 -0500

    Configurando los botones y añidiendo imagen alura+oracle en el archivo HTML y CSS, creando funciones en el archivo java.js

--- JS/java.js
+++ JS/java.js
@@ -1,3 +1,52 @@
-Function encriptar(){
+function encriptar(){
+    let texto = document.getElementById("texto").value;
+    let commentTitle = document.getElementById("titulo");
+    let parrafo = document.getElementById("parrafo");
+    let imagen = document.getElementById("secund-img");
+
+    let textoSecreto = texto 
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat")
+
+    if (texto.length !=0) {
+        document.getElementById("texto").value = textoSecreto;
+        commentTitle.textContent = "Texto encriptado con éxito";
+        parrafo.textContent = "";
+        imagen.src = "./IMG/encriptado.jpg"
+    }else{
+        imagen.src = "./IMG/4621080_2431962-removebg-preview.png";
+        commentTitle.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingrese el texto que desea Encriptar o Desencriptar";
+        alert("Debes ingresar algún texto");
+    }
+}
+
+function desencriptar () {
+    let texto = document.getElementById("texto").value;
+    let commentTitle = document.getElementById("titulo");
+    let parrafo = document.getElementById("parrafo");
+    let imagen = document.getElementById("secund-img");
+
+    let textoSecreto = texto 
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u")
+
+        if (texto.length !=0) {
+            document.getElementById("texto").value = textoSecreto;
+            commentTitle.textContent = "Texto desencriptado con éxito";
+            parrafo.textContent = "";
+            imagen.src = "./IMG/desencriptado.png"
+        }else{
+            imagen.src = "./IMG/4621080_2431962-removebg-preview.png";
+            commentTitle.textContent = "Ningun mensaje fue encontrado";
+            parrafo.textContent = "Ingrese el texto que desea Encriptar o Desencriptar";
+            alert("Debes ingresar algún texto");
+        }
     
 }
